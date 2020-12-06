0ba59bd80e8fe35efbe8779604b3de3d452b5a74
Douman <douman@gmx.se>
2020-12-06 13:09:59 +0900

    alpha.2 Extend APIs get/remove

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@
 use std::collections::BTreeMap;
 use xxhash_rust::xxh3::xxh3_128;
 
+use core::ptr;
+
 mod enc;
 
 ///Secure storage API
@@ -71,29 +73,85 @@ impl Store {
         self.inner.len()
     }
 
+    fn inner_get_to(&self, key: u128, dest: &mut [u8]) -> Result<usize, ()> {
+        match self.inner.get(&key) {
+            Some(value) => {
+                if value.len() > dest.len() {
+                    return Ok(0);
+                }
+
+                unsafe {
+                    ptr::copy_nonoverlapping(value.as_ptr(), dest.as_mut_ptr(), value.len());
+                }
+
+                match self.enc.decrypt(key, &mut dest[..value.len()]) {
+                    Some(written) => {
+                        Ok(written.len())
+                    },
+                    None => Err(())
+                }
+            },
+            None => Err(())
+        }
+    }
+
+    fn inner_get_to_vec(&self, key: u128, dest: &mut Vec<u8>) -> Result<usize, ()> {
+        match self.inner.get(&key) {
+            Some(value) => {
+                dest.truncate(0);
+                dest.extend_from_slice(&value);
+                match self.enc.decrypt(key, dest) {
+                    Some(written) => {
+                        let len = written.len();
+                        dest.truncate(len);
+                        dest.shrink_to_fit();
+                        Ok(dest.len())
+                    },
+                    None => Err(())
+                }
+            },
+            None => Err(()),
+        }
+    }
+
+    #[inline]
+    ///Retrieves value for `key`, storing decrypted value in `dest`.
+    ///
+    ///Returns `Err` when key doesn't exist or user has no permission to read it.
+    ///Otherwise returns number of bytes written, or '0' in case of insufficient storage.
+    pub fn get_to(&self, key: &[u8], dest: &mut [u8]) -> Result<usize, ()> {
+        let key = xxh3_128(key).to_le();
+
+        self.inner_get_to(key, dest)
+    }
+
+    #[inline]
+    ///Retrieves value for `key` to store in `dest`, resulting in it being overwritten.
+    ///
+    ///Returns `Err` when key doesn't exist or user has no permission to read it.
+    ///Otherwise returns number of bytes written.
+    pub fn get_to_vec(&self, key: &[u8], dest: &mut Vec<u8>) -> Result<usize, ()> {
+        let key = xxh3_128(key).to_le();
+
+        self.inner_get_to_vec(key, dest)
+    }
+
+    #[inline]
     ///Retrieves value for `key`
     ///
     ///Returns `None` if decryption failed.
     pub fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
-        let key = xxh3_128(key).to_le();
-
-        self.inner.get(&key).and_then(|value| {
-            let mut value = value.clone();
-            match self.enc.decrypt(key, &mut value) {
-                Some(written) => {
-                    let len = written.len();
-                    drop(written);
-                    value.truncate(len);
-                    value.shrink_to_fit();
-                    Some(value)
-                },
-                None => None
-            }
-        })
+        let mut result = Vec::new();
+        match self.get_to_vec(key, &mut result) {
+            Ok(_) => Some(result),
+            Err(_) => None,
+        }
     }
 
     ///Inserts new owned `value` for `key`, returning previous one, if any.
     pub fn insert_owned(&mut self, key: &[u8], mut value: Vec<u8>) -> Option<Vec<u8>> {
+        assert_ne!(value.len(), 0);
+
         let key = xxh3_128(key).to_le();
 
         assert!(self.enc.encrypt(key, &mut value));
@@ -102,7 +160,6 @@ impl Store {
             match self.enc.decrypt(key, &mut value) {
                 Some(written) => {
                     let len = written.len();
-                    drop(written);
                     value.truncate(len);
                     value.shrink_to_fit();
                     Some(value)
@@ -118,25 +175,57 @@ impl Store {
         self.insert_owned(key, value.to_owned())
     }
 
-    ///Removes `key`, returning previous value, if any.
-    pub fn remove(&mut self, key: &[u8]) -> Option<Vec<u8>> {
+    ///Extracts value under `key` to specified `dest`
+    ///
+    ///Returns `Err` when key doesn't exist, or user has no permission to read it.
+    ///Otherwise returns number of bytes written, or '0' in case of insufficient storage.
+    ///
+    ///Note that value is removed only if `Ok(size) >= Ok(1)`
+    pub fn remove_to(&mut self, key: &[u8], dest: &mut [u8]) -> Result<usize, ()> {
         let key = xxh3_128(key).to_le();
-        self.inner.remove(&key).and_then(|mut value| {
-            match self.enc.decrypt(key, &mut value) {
-                Some(written) => {
-                    let len = written.len();
-                    drop(written);
-                    value.truncate(len);
-                    value.shrink_to_fit();
-                    Some(value)
-                },
-                None => None
-            }
-        })
+
+        match self.inner_get_to(key, dest) {
+            Ok(0) => Ok(0),
+            Ok(result) => {
+                let _ = self.inner.remove(&key);
+                Ok(result)
+            },
+            Err(_) => Err(()),
+        }
+    }
+
+    ///Extracts value under `key` to specified `dest`
+    ///
+    ///Returns `Err` when key doesn't exist, or user has no permission to read it.
+    ///Otherwise returns number of bytes written.
+    pub fn remove_to_vec(&mut self, key: &[u8], dest: &mut Vec<u8>) -> Result<usize, ()> {
+        let key = xxh3_128(key).to_le();
+
+        match self.inner_get_to_vec(key, dest) {
+            Ok(result) => {
+                let _ = self.inner.remove(&key);
+                Ok(result)
+            },
+            Err(_) => Err(()),
+        }
+    }
+
+    #[inline]
+    ///Removes `key`, returning previous value, if any.
+    ///
+    ///Failing to decrypt, doesn't remove value.
+    pub fn remove(&mut self, key: &[u8]) -> Option<Vec<u8>> {
+        let mut result = Vec::new();
+        match self.remove_to_vec(key, &mut result) {
+            Ok(_) => Some(result),
+            Err(_) => None,
+        }
     }
 
     #[inline]
     ///Removes `key`, returning whether it was set previously.
+    ///
+    ///Note that it only removes value, without checking if you can read it.
     pub fn remove_key(&mut self, key: &[u8]) -> bool {
         let key = xxh3_128(key).to_le();
         self.inner.remove(&key).is_some()
--- tests/store.rs
+++ tests/store.rs
@@ -6,12 +6,20 @@ const PASS: &[u8] = b"pass";
 
 #[test]
 fn should_securely_manage_values() {
+    let mut owned_bytes = Vec::new();
+    let mut bytes: [u8; 1024] = [0; 1024];
     let mut store = Store::new(USER, PASS);
 
     assert_eq!(store.len(), 0);
     assert!(store.insert(b"1", PASS).is_none());
     assert_eq!(store.len(), 1);
     assert_eq!(store.get(b"1").unwrap(), PASS);
+    assert_eq!(store.get_to(b"1", &mut bytes).unwrap(), PASS.len());
+    assert_eq!(bytes[..PASS.len()], PASS[..]);
+    assert_eq!(store.get_to_vec(b"1", &mut owned_bytes).unwrap(), PASS.len());
+    assert_eq!(owned_bytes.as_slice(), &PASS[..]);
+    assert_eq!(store.get_to_vec(b"1", &mut owned_bytes).unwrap(), PASS.len());
+    assert_eq!(owned_bytes.as_slice(), &PASS[..]);
 
     assert_eq!(store.insert(b"1", USER).unwrap(), PASS);
     assert_eq!(store.len(), 1);
@@ -31,13 +39,23 @@ fn should_securely_manage_values() {
 
     assert!(store.get(b"1").is_none());
     assert!(store.get(b"2").is_none());
+    assert_eq!(store.remove_to(b"2", &mut []).unwrap(), 0);
+    assert_eq!(store.len(), 2);
     assert!(store.remove(b"2").is_none());
+    assert_eq!(store.len(), 2);
+    assert!(store.insert(b"2", PASS).is_none());
+    bytes = [0; 1024];
+    assert_eq!(store.remove_to(b"2", &mut bytes).unwrap(), PASS.len());
+    assert_eq!(bytes[..PASS.len()], PASS[..]);
     assert_eq!(store.len(), 1);
 
     let store = store.into_inner();
     let mut store = Store::from_inner(store, b"WRONG", PASS);
 
     assert!(store.get(b"1").is_none());
+    assert!(store.get_to(b"1", &mut bytes).is_err());
+    assert!(store.remove_to(b"1", &mut bytes).is_err());
+    assert_eq!(store.len(), 1);
     assert!(store.remove(b"1").is_none());
-    assert_eq!(store.len(), 0);
+    assert_eq!(store.len(), 1);
 }
